ff655647af570585085c36658b62462b2a4ca279
Jakub Socha <jakub.socha2@tomtom.com>
2022-06-27 21:52:15 +0200

    docs(terraform): Create deployment plan

---
+++ infrastructure/terraform/base/main.tf
@@ -0,0 +1,8 @@
+//VPC public + private (terraform registry?)
+
+//ECS cluster
+//ECS Service
+//ECS task definition
+//Fargate vs EC2? Terraform registry for Fargate?
+
+//Rotue53 -> terraform registry?
